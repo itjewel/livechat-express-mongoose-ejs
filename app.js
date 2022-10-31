aaa5b89748ca9015ff1470084e48d8bbf09a7739
md.jewel mia <md.jewelmia52@gmail.com>
2022-10-31 18:45:43 +0600

    chatting board and websocket integration

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-// External Imports
+// external imports
 const express = require("express");
+const http = require("http");
 const dotenv = require("dotenv");
-const path = require("path");
 const mongoose = require("mongoose");
+const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
-// Internal Imports
-const {
-  errorHandler,
-  notFoundHandler,
-} = require("./middlewares/common/errorhandler");
+// internal imports
 const loginRouter = require("./router/loginRouter");
 const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter");
 
+// internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
+
 const app = express();
-// request process
-app.use(express.json());
-// query param pass
-app.use(express.urlencoded({ extends: true }));
+const server = http.createServer(app);
 dotenv.config();
 
-// Database Connection
-main().catch((err) => console.log(err));
-async function main() {
-  try {
-    await mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING);
-  console.log("db connection success");
-  } catch (error) {
-    console.log("db connection failed");
-  }
-  
-}
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
 
-// View engine
+// set comment as app locals
+app.locals.moment = moment;
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch((err) => console.log(err));
+
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
 app.set("view engine", "ejs");
 
 // set static folder
@@ -47,15 +56,12 @@ app.use("/", loginRouter);
 app.use("/users", usersRouter);
 app.use("/inbox", inboxRouter);
 
-// 404 not found handeler
+// 404 not found handler
 app.use(notFoundHandler);
 
-// Default error handeler
+// common error handler
 app.use(errorHandler);
 
-// server running
-app.set("port", process.env.PORT || 5000);
-
-app.listen(app.get("port"), () => {
-  console.log("Express server listening on port " + app.get("port"));
+server.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
 });
