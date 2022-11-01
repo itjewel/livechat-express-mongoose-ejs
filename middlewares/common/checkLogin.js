2d904323c7b1438684dcb0af547078bb81b96621
md.jewel mia <md.jewelmia52@gmail.com>
2022-11-01 18:04:10 +0600

    Role authentication added

--- middlewares/common/checkLogin.js
+++ middlewares/common/checkLogin.js
@@ -50,7 +50,30 @@ const redirectLoggedIn = function (req, res, next) {
   }
 };
 
+
+// guard to protect routes that need role based authorization
+function requireRole(role) {
+  return function (req, res, next) {
+    if (req.user.role && role.includes(req.user.role)) {
+      next();
+    } else {
+      if (res.locals.html) {
+        next(createError(401, "You are not authorized to access this page!"));
+      } else {
+        res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are not authorized!",
+            },
+          },
+        });
+      }
+    }
+  };
+}
+
 module.exports = {
   checkLogin,
   redirectLoggedIn,
+  requireRole,
 };
